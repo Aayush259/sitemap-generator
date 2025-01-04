6159a0479f64dbaf8b9dad9ea838d27f1d9bf8ed
Aayush <aayushk4980@gmail.com>
2025-01-04 22:04:42 +0530

    Init

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Sitemap Generator",
+    description: "Generate a sitemap for your website",
+};
+
+export default function Page() {
+    return (
+        <div className="overflow-hidden font-[family-name:var(--font-geist-sans)]">
+
+        </div>
+    );
+};
