7f00468e8353c16ea0f8f73577064d54fdaa69dc
Aayush <aayushk4980@gmail.com>
2025-01-09 00:13:52 +0530

    Update title and meta description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Home from "@/components/Home";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Sitemap Generator",
-    description: "Generate a sitemap for your website",
+    title: "Sitemap Generator & Broken Links Checker",
+    description: "Generate sitemaps and easily identify broken links on your website. Improve your site's SEO and ensure proper indexing by search engines.",
 };
 
 export default function Page() {
